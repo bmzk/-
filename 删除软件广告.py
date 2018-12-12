73193db07236ca6bc682fe0302225269bd299501
bmzk <bmzk123@163.com>
2018-12-12 13:25:49 +0800

    Create 删除软件广告.py

---
+++ 删除软件广告.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8  -*-
+import turtle
+bob = turtle.Turtle()
+print(bob)
+#turtle.mainloop()
+bob.fd(100)
+print(1)
+bob.fd(100)
+print(2)
+bob.lt(90)
+print(3)
+bob.fd(100)
+print(4)
+for i in range(4):
+ bob.fd(100)
+ bob.lt(90)
+a=1
+for i in range(44):
+ print(a)
+ a=a+1
+x=1
+for x in range (11):
+ print(x)
+ s=input('What...is your name?\n')
+ print("....................................................................................................")
+ print(s  + x )
+print("1212....................................................................................................")
+s=input('What...is your name?\n 11232154644984898979')
