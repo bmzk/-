5248b5a04819dfeb67922099440c06f00482aaef
bmzk <37799175+bmzk@users.noreply.github.com>
2019-04-09 17:25:33 +0800

    1.1

--- 删除软件广告.py
+++ 删除软件广告.py
@@ -2,126 +2,127 @@
 import os
 import random
 import time
-
-#q
-'广告文件列表'
-adlist={''}
-'腾讯'
-adlist.add
+'''腾讯'''
 Tencent='C:\\Users\\asd\\AppData\\Roaming\\Tencent\\'
-adlist.add(Tencent+"QQMicroGameBox\\Launch.exe")
-adlist.add(Tencent+'QQMicroGameBox\\1.1.4.5\\QQMGameBoxUpdater.exe')
-adlist.add(Tencent+"QQMicroGameBox\\1.1.4.5\\QQMicroGameBox.exe")
-adlist.add(Tencent+"QQMicroGameBox\\1.1.4.5\\QQMicroGameBoxTray.exe")
-adlist.add(Tencent+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(Tencent+'QQMicroGameBox\\1.1.4.5\\WebBrowserProcess.exe')
-adlist.add(Tencent+'QQMicroGameBox\\1.1.4.5\\WebServer.exe')
-
-
 Tencent2='C:\\Program Files (x86)\\Tencent\\QQMicroGameBoxService\\1.0.5.2\\'
-adlist.add(Tencent2+'plugin.exe')
-adlist.add(Tencent2+'QQMGBWebserver.exe')
-adlist.add(Tencent2+'QQMicroGameBoxService.exe')
-adlist.add(Tencent2+'QQMicroGameBoxServiceUpdate.exe')
-adlist.add(Tencent2+'QQMicroGameBoxServiceUpdate.exe')
-'wps'
-wps='C:\\Program Files (x86)\\WPS Office\\'
-adlist.add(wps+'10.1.0.6747\\wtoolex\\desktoptip.exe')
-adlist.add(wps+'10.1.0.6747\\wtoolex\\wpsnotify.exe')
-adlist.add(wps+'10.1.0.6747\\wtoolex\\wpsupdate.exe')
-adlist.add(wps+'10.1.0.6747\\wtoolex\\updateself.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
-adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+#'''广告文件列表,集合是一个无序的不重复元素序列,可以避免面使用列表时出现重复元素'''
+adlist={
+Tencent+"QQMicroGameBox\\Launch.exe",
+# Tencent+'QQMicroGameBox\\1.1.4.5\\QQMGameBoxUpdater.exe',
+# Tencent+"QQMicroGameBox\\1.1.4.5\\QQMicroGameBox.exe",
+# Tencent+"QQMicroGameBox\\1.1.4.5\\QQMicroGameBoxTray.exe",
+# Tencent+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe',
+# Tencent+'QQMicroGameBox\\1.1.4.5\\WebBrowserProcess.exe',
+# Tencent+'QQMicroGameBox\\1.1.4.5\\WebServer.exe'
+}
 
-errorlist=[]
-noFind=[]
+# adlist.add(Tencent2+'plugin.exe')
+# adlist.add(Tencent2+'QQMGBWebserver.exe')
+# adlist.add(Tencent2+'QQMicroGameBoxService.exe')
+# adlist.add(Tencent2+'QQMicroGameBoxServiceUpdate.exe')
+# adlist.add(Tencent2+'QQMicroGameBoxServiceUpdate.exe')
+# 'wps'
+# wps='C:\\Program Files (x86)\\WPS Office\\'
+# adlist.add(wps+'10.1.0.6747\\wtoolex\\desktoptip.exe')
+# adlist.add(wps+'10.1.0.6747\\wtoolex\\wpsnotify.exe')
+# adlist.add(wps+'10.1.0.6747\\wtoolex\\wpsupdate.exe')
+# adlist.add(wps+'10.1.0.6747\\wtoolex\\updateself.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+# adlist.add(wps+'QQMicroGameBox\\1.1.4.5\\Toolkit.exe')
+adlist.add('C:\\Users\\asd\\Desktop\\asd.txt')
+adlist.add('C:\\Users\\asd\\Desktop\\学院ppt制作模版2.JPG')
+
+
+noFindFile=[]
 existAd=[]
+canNotDeleteFile=[]
+errorlist=[]
+successDeleteFile=[]
+unsuccessDeleteFile=[]
 def delete(adList:list):
-    for i in adList:
-        print("要删除文件：",i)
+    '''sub_删除广告程序,无返回值 \n
+    调用函数 fgx()'''
+    for i in adList:#显示要删除的文件
+        #print("要删除文件：",i)
+        pass
     fgx()
     for i in adList:
         if os.path.exists(i):
             existAd.append(i)
             renameFile(i)
+            #print(i)
         else:
             print(getTime(time.time()),'文件未找到:',i)
-            noFind.append(i)
+            noFindFile.append(i)
     fgx()
-    checkResult(existAd)
+    checkResult()
     fgx()
-
-
-
 
 def renameFile(file):
     '''对文件进行重命名'''
-    fileId=0
     t_current=time.time()
-    while os.path.exists(file[:-4]+getTime(t_current,0,1)+'-'+str(fileId)):
-        fileId+=1
+    t_current=20190409
     try:
-        os.rename(file, file[:-4]+getTime(t_current,0,1)+'-'+str(fileId))
-        print(getTime(time.time()),'已删除:',file)
-    except PermissionError:
-        print(getTime(time.time()),'文件被占用或权限不足:',file)
-        errorlist.append(file)
+        a=file
+        b=file[:-4]+getTime(t_current)+'.txt'
+        #print('00000000000000000000000000',a,b)
+        print('successDeleteFile',successDeleteFile)
+        print( os.path.basename(file))
+        os.rename(a, b)
+        #os.rename(file, file[:-4]+getTime(t_current))
+        successDeleteFile.append(file)
+        
+    # except PermissionError:
+    #     print(getTime(time.time()),'文件被占用或权限不足:',file)
+    #     canNotDeleteFile.append(file)
+    # except IOError:
+    #     print('IOError', file)
+    #     if os.path.exists(file):
+    #         print('IOError000000000000000000000000000000')
+    #     errorlist.append(file)
     except:
         print(getTime(time.time()),'删除文件出现错误:',file)
-    
-      
-def getTime(t_current=0.0,mode=3,n=3):      
-    t=time.localtime(t_current)
-    ms=int((t_current-int(t_current))*1000)
-    if mode==0:
-        returnValue=time.strftime('%Y/%m/%d %H:%M:%S',t)+'::'+str(ms)+' '*n
-    elif mode==1:
-        returnValue=time.strftime('%Y/%m/%d',t)+' '*n
-    elif mode==2:
-        returnValue=time.strftime('%H:%M:%S',t)+' '*n
-    elif mode==3:
-        returnValue=time.strftime('%H:%M:%S',t)+'::'+str(ms)+' '*n
-    elif mode==4:
-        returnValue=time.strftime('%Y/%m/%d %H:%M:%S',t)+'::'+str(ms)+' '*n
-    elif mode==5:
-        returnValue=time.strftime('%Y%m%d%H%M%S',t)+' '*n
-    elif mode==6:
-        returnValue=time.strftime('%Y/%m/%d %H:%M:%S',t)+'::'+str(ms)+' '*n
-    else:
-        returnValue=time.strftime('%Y/%m/%d %H:%M:%S',t)+'::'+str(ms)+' '*n
-    return returnValue
-def checkResult(adList):
-    deleted=[]
-    noDeleted=[]
-    for ad in adList:
+        errorlist.append(file)
+
+def getTime(t=0.0):      
+    '''获得14个数字表示的时间字符串,如2019040911203'''
+    return time.strftime('%Y%m%d%H%M%S',time.localtime(t))
+def checkResult():
+    for ad in successDeleteFile:
         if os.path.exists(ad):
-            noDeleted.append(ad)
+            unsuccessDeleteFile.append(ad)
+            successDeleteFile.remove(ad)
         else:
-            deleted.append(ad)
+            newfile = open(ad,'a+' )
+            newfile.write('012345698761416raegfgg')
+            newfile.close
     fgx()
+    print('  要删除文件: ',len(adlist))
+    print('实际存在文件: ',len(existAd))
+    print('  不存在文件: ',len(noFindFile))
     fgx()
-    print('要删除',len(adList)+len(noFind),'个文件,其中',len(deleted),'个文件已经删除.',len(noFind),'个文件未找到,',len(errorlist),'个文件被占用或权限不足.')
+    print('成功删除: ',len(successDeleteFile))
+    print('未 删 除: ',len(unsuccessDeleteFile))
+    print('出现错误:',len(errorlist))
     fgx()
-    print(getTime(time.time()),'已经删除文件:')
-    for i in deleted:
+    for i in noFindFile:
+        print('未 找 到: ',i)
+    fgx()
+    for i in successDeleteFile:
         print('成功删除: ',i)
     fgx()
-    print(getTime(time.time()),'未找到文件:')
-    for i in noFind:
-        print('未找到: ',i)
+    for i in unsuccessDeleteFile:
+        print('未 删 除: ',i)
     fgx()
-    print(getTime(time.time()),'未能删除文件:')
-    for i in noDeleted:
-        print('未删除: ',i)
-
+    for i in errorlist:
+        print('出现错误: ',i)
 
 def fgx(n=22,tips=""):
+    '''分割线,为使输出更美观,输出一行 - 符号\n'''
     print(tips,'--'*22)
 
 delete(adlist)
